bdab74a682e59d8c5a9e9a77c72b228db1f8fc42
rhinoandre <rhinoandre@gmail.com>
2024-07-04 14:37:50 -0300

    feat: unblock pixels when removing the color

--- src/app/components/PixelPainter.js
+++ src/app/components/PixelPainter.js
@@ -25,10 +25,10 @@ function updatePixelRef({
    *
    */
   if (isBeadsArt) {
-    paintedPixels = updatePixelRef({paintedPixels, x: x - 1, y, color: null});
-    paintedPixels = updatePixelRef({paintedPixels, x: x + 1, y, color: null});
-    paintedPixels = updatePixelRef({paintedPixels, x, y: y - 1, color: null});
-    paintedPixels = updatePixelRef({paintedPixels, x, y: y + 1, color: null});
+    paintedPixels = updatePixelRef({ paintedPixels, x: x - 1, y, color: null });
+    paintedPixels = updatePixelRef({ paintedPixels, x: x + 1, y, color: null });
+    paintedPixels = updatePixelRef({ paintedPixels, x, y: y - 1, color: null });
+    paintedPixels = updatePixelRef({ paintedPixels, x, y: y + 1, color: null });
   }
 
   return {
@@ -36,6 +36,52 @@ function updatePixelRef({
     [x]: {
       ...paintedPixels[x],
       [y]: color
+    }
+  };
+}
+
+/**
+   * For beads art, the previous pixel and the ones right above and below it should be NULL
+   * Selected: x:2, Y:B
+   * X will be blocked pixels
+   *   0 1 2 3 4 5 👈 X axis
+   * a 0 0 X 0 0 X
+   * b 0 X 1 X X 1
+   * c 0 0 X 0 0 X
+   * d 0 X 1 X 0 0
+   * 👆🏼
+   * Y axis
+   */
+function unPaintPixel({ paintedPixels, x, y, isBeadsArt }) {
+  // To unpaint the pixel I also need to unblock the pixels around it,
+  // unless the blocked pixel have another pixel painted on adjacent (top/bottom/left/right) of it.
+  // So to remove the pixel at b/5 I need to unblock the pixels: a/5, c/5, b/4
+  // But for the pixel at b/2 I can only unblock the pixels: a/2, b/1, b/3
+  // as the pixel at c/2 has a painted pixel adjacent painted on d/2
+  if (isBeadsArt) {
+    // 2 up and 1 up 1 left
+    if(!paintedPixels[x-2]?.[y] && !paintedPixels[x-1]?.[y-1]) {
+      paintedPixels = updatePixelRef({ paintedPixels, x: x - 1, y });
+    }
+    // 2 down and 1 down 1 right
+    if(!paintedPixels[x+2]?.[y] && !paintedPixels[x+1]?.[y+1]) {
+      paintedPixels = updatePixelRef({ paintedPixels, x: x + 1, y });
+    }
+    // 2 left and 1 up 1 left
+    if(!paintedPixels[x]?.[y-2] && !paintedPixels[x-1]?.[y-1]) {
+      paintedPixels = updatePixelRef({ paintedPixels, x, y: y - 1 });
+    }
+    // 2 right and 1 down 1 right
+    if(!paintedPixels[x]?.[y+2] && !paintedPixels[x+1]?.[y+1]) {
+      paintedPixels = updatePixelRef({ paintedPixels, x, y: y + 1 });
+    }
+  }
+
+  return {
+    ...paintedPixels,
+    [x]: {
+      ...paintedPixels[x],
+      [y]: undefined
     }
   };
 }
@@ -58,13 +104,13 @@ export function PixelPainter({
   const paintPixel = (e, x, y) => {
     if (isMouseDown || e.type === 'click') {
       const paintedPixels = paintedPixelsRef.current;
-      // if beads at, check if the pixel isn't blocked (null)
+      // if beads art mode, check if the pixel isn't blocked (null)
       if (isBeadsArt && isPixelBlocked(paintedPixels, x, y)) {
         return;
       }
       if (e.type === 'click' && isSelectedPixel(paintedPixels, x, y) && colorSelected === paintedPixels[x][y]) {
         e.target.style.backgroundColor = '';
-        paintedPixelsRef.current = updatePixelRef({
+        paintedPixelsRef.current = unPaintPixel({
           x, y,
           paintedPixels,
           isBeadsArt
